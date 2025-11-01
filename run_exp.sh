e2ef4237f470912e3e497af465f737a9532d60ba
Siddhesh <talktosiddhesh.shinde@gmail.com>
2025-11-01 21:29:05 +0530

    added run.sh script and results

--- run_exp.sh
+++ run_exp.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-DB_CORE=0
-SERVER_CORE=1
-LOADGEN_CORES=2
 
-echo "Starting server..."
-taskset -c $SERVER_CORE ./server &
 
-sleep 1
-echo "Starting load generator..."
-taskset -c $LOADGEN_CORES ./loadgen 10 60 get_all
+WORKLOAD="get_popular"
+DURATION=60
+THREADS=(8)
+
+RESULTS_DIR="results"
+
+echo "Starting server on core: 1"
+taskset -c 1 ./server &
+SERVER_PID=$!
+echo "Server started with PID $SERVER_PID"
+
+
+sleep 5
+
+
+for THREADS in "${THREADS[@]}"; do
+    echo "--- Running $WORKLOAD with $THREADS threads on core: 2 ---"
+
+    taskset -c 2 ./loadgen $THREADS $DURATION $WORKLOAD > $RESULTS_DIR/${WORKLOAD}/${THREADS}threads.txt
+    
+    echo "Results saved to $RESULTS_DIR/${THREADS}_threads.txt"
+    sleep 10
+
+done
+
+kill $SERVER_PID
+echo "Server stopped."
